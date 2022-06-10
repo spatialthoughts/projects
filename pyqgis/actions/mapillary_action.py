6283a4c11a213699ab341114935f19c899a9fd5a
Ujaval Gandhi <ujaval@spatialthoughts.com>
2022-06-10 22:35:27 +0530

    Update mapillary_action.py

--- pyqgis/actions/mapillary_action.py
+++ pyqgis/actions/mapillary_action.py
@@ -3,22 +3,23 @@ from qgis.PyQt.QtCore import QUrl
 from qgis.PyQt.QtWebKitWidgets import QWebView
 from qgis.utils import iface
 
-parameters = {
-    'client_id': '<REPLACE WITH YOUR CLIENT_ID>',
-    'closeto': '{},{}'.format([%$x%],[%$y%]),
-    'per_page': 1,
-    'radius': 1000 
-}
-response = requests.get(
-        'https://a.mapillary.com/v3/images', params=parameters)
+# https://www.mapillary.com/developer/api-documentation#image
 
+parameters = {
+    'access_token': '<access token>',
+    'bbox': '{},{},{},{}'.format([%$x%]-0.001,[%$y%]-0.001, [%$x%]+0.001, [%$y%]+0.001),
+    'fields': 'thumb_1024_url',
+	'limit': 1
+}
+
+response = requests.get(
+        'https://graph.mapillary.com/images', params=parameters)
 if response.status_code == 200:
-    data = response.json()
-    if data['features']:
-        key = data['features'][0]['properties']['key']
-        url = 'https://images.mapillary.com/{}/thumb-640.jpg'.format(key)
+    data_json = response.json()
+    if data_json['data']:
+        url = data_json['data'][0]['thumb_1024_url']
         myWV = QWebView(None)
         myWV.load(QUrl(url))
         myWV.show()
     else:
-        iface.messageBar().pushMessage('No images found')
+        qgis.utils.iface.messageBar().pushMessage('No images found')
